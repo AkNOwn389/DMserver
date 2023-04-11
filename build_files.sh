3e06d1ef7405d069b191643378a77d67289b3d09
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-12 05:11:58 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START BOSS DARIUS"
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
