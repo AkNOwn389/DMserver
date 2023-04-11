555341a720f657fccdef5f92a015acb284a20fac
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-12 07:21:43 +0800

    .

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,30 @@
-echo "BUILD START"
+echo "BUILD START BOSS DARIUS"
+python3 -m pipenv install
+python3.9 -m pipenv shell
+python3 -m pip install --upgrade setuptools
+python3.9 manage.py collectstatic --noinput
+python3.9 pip install mysql-python
+python3.9 -m pip install configparser
+python3.9 -m pip install mysqlclient
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install daphne
+python3.9 -m pip install django
+python3.9 -m pip install djangorestframework-simplejwt
+python3.9 -m pip install django-cors-headers
+python3.9 -m pip install django-cloudinary-storage
+python3.9 -m pip install cloudinary
+python3.9 -m pip install django_extensions
+python3.9 -m pip install django-imagekit
+python3.9 -m pip install Pillow
+python3.9 -m pip install djangorestframework
+python3.9 -m pip install drf-yasg
+python3.9 -m pip install PyJWT
+python3.9 -m pip install shortuuid
+python3.9 -m pip install channels
+python3.9 -m pip install imageio
+python3.9 -m pip install pilkit
+python3.9 -m pip install websocket-client
+python3.9 -m pip install mysql-python
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f"{BASE_DIR}/db.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ATOMIC_REQUESTS': True,
     }
 }
