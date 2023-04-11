23c7fecbd3ca2766d39bf274779dcb1676894da1
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-12 06:24:53 +0800

    .

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,13 @@
 echo "BUILD START BOSS DARIUS"
 python3 -m pip install --user --no-cache-dir google-cloud-bigquery
 python3 -m pip install --upgrade setuptools
-pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 apt-get install python-dev
-sudo apt-get install mysql-server
-sudo apt-get install libmysqlclient-dev
-sudo apt-get install libmariadbclient-dev
-sudo apt-get install libmysqlclient-dev
+python3.9 -m pip install django
 python3.9 pip install --upgrade-pip
 python3.9 pip install mysql-python
 python3.9 -m pip install mysqlclient
 python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install django
 python3.9 -m pip install _sqlite3
 python3.9 -m pip install daphne
 python3.9 -m pip install djangorestframework-simplejwt
