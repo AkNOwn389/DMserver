cf648680f00f610fe13273fc86874975e2efc314
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-12 05:19:07 +0800

    ,

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@ echo "BUILD START BOSS DARIUS"
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install django
 python3.9 -m pip install daphne
+python3.9 -m pip install djangorestframework-simplejwt
+python3.9 -m pip install django-cors-headers
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
