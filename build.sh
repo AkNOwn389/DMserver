0b66a481cfee3722e49942fffc84abdfdae8b528
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-12 19:05:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,4 +29,3 @@ echo "Make Migrations"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
