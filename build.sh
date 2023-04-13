1848b93522b8bc8ddc28eef78b23d182f3a39d1f
black <63425602+AkNOwn389@users.noreply.github.com>
2023-04-13 15:37:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,4 @@ echo "BUILD START BOSS DARIUS"
 python3.9 -m pipenv shell
 python3.9 -m pip install -r requirements.txt
 echo "BUILD END"
-echo "Make Migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 echo "Collect Static..."
